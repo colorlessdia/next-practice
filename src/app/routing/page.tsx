408b870e64660bbc2b71d590351b5ae77c14c70f
colorlessdia <colorlessdia@gmail.com>
2024-08-04 12:36:11 +0900

    [feat] default routing

---
+++ src/app/routing/page.tsx
@@ -0,0 +1,13 @@
+const Routing = () => {
+  return (
+    <>
+      <h2>기본 라우팅</h2>
+      <ol>
+        <li>app 이하 디렉토리를 생성(app/routing)</li>
+        <li>해당 디렉토리에 page 파일을 생성</li>
+      </ol>
+    </>
+  );
+};
+
+export default Routing;
