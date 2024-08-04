eabfa107b5a6cd2c2fd9bd9bc26af2c69c92300e
colorlessdia <colorlessdia@gmail.com>
2024-08-04 16:29:54 +0900

    [feat] GNB active link

--- src/components/Header/GNB/LinkItem/LinkItem.tsx
+++ src/components/Header/GNB/LinkItem/LinkItem.tsx
@@ -1,14 +1,32 @@
-import { GNBLinkItem } from "@/types/GNBType";
-import Link from "next/link";
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { GNBLinkItem } from '@/types/GNBType';
+import Link from 'next/link';
 
 type LinkItemProps = {
   link: GNBLinkItem;
 };
 
 const LinkItem = ({ link }: LinkItemProps) => {
+  const pathname = usePathname();
+  const isActive = pathname === link.href ? true : false;
+
   return (
-    <li key={link.href}>
-      <Link href={link.href}>{link.title}</Link>
+    <li
+      key={link.href}
+      style={{
+        fontWeight: isActive ? 700 : 500,
+      }}
+    >
+      <Link
+        href={link.href}
+        style={{
+          color: isActive ? '#333' : '#666',
+        }}
+      >
+        {link.title}
+      </Link>
     </li>
   );
 };
