601bd654a7ec63c4c12bd2c8a4ee86e208b1291f
colorlessdia <colorlessdia@gmail.com>
2024-08-04 12:43:10 +0900

    [feat] nested-routes

---
+++ src/app/routing/nested-routes/page.tsx
@@ -0,0 +1,13 @@
+const NestedRoutes = () => {
+  return (
+    <>
+      <h2>중첩 라우팅</h2>
+      <ol>
+        <li>라우팅 디렉토리 하위에 디렉토리를 생성(app/routing/nested-routes)</li>
+        <li>해당 디렉토리에 page 파일을 생성</li>
+      </ol>
+    </>
+  );
+};
+
+export default NestedRoutes;
--- src/app/routing/page.tsx
+++ src/app/routing/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Routing = () => {
   return (
     <>
@@ -6,6 +8,7 @@ const Routing = () => {
         <li>app 이하 디렉토리를 생성(app/routing)</li>
         <li>해당 디렉토리에 page 파일을 생성</li>
       </ol>
+      <Link href='/routing/nested-routes'>중첩 라우팅</Link>
     </>
   );
 };
